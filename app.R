ffbd5709b11f5462860ce30317efe46d7c1669b8
Tjark Gerken <tjark.gerken@accenture.com>
2023-05-26 11:13:28 +0200

    init

---
+++ app.R
@@ -0,0 +1,24 @@
+# load the required packages
+
+
+library(shiny)
+require(shinydashboard)
+library(plotly)
+library(dplyr)
+
+
+
+header <- dashboardHeader(title = "Einzelhandelsumsätze")
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Filter", selectInput("Filliale", label = "Wählen Sie eine Filiale",
+                                   choices = c("Alle",'A', 'B', "C"), selected = 'Alle' ))
+  )
+)
+
+body <- dashboardBody()
+ui <- dashboardPage(title = 'Einzelhandelsumsätze', header, sidebar, body, skin='blue')
+server <- function(input, output) { }
+
+shinyApp(ui,  server)
